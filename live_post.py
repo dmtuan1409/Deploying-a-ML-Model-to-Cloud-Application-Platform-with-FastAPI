b2b865e88c3642c4f461b89b7d61e0b888d0e5fa
dttuan99 <thichhocchui1999@gmail.com>
2023-06-29 22:49:15 +0700

    update final code

---
+++ live_post.py
@@ -0,0 +1,27 @@
+#POSTS to the API
+
+import requests
+
+url = "https://census-income-predict.onrender.com"
+sample = {
+            "age": 35,
+            "workclass": "Private",
+            "fnlgt": 77516,
+            "education": "HS-grad",
+            "education_num": 9,
+            "marital_status": "Divorced",
+            "occupation": "Handlers-cleaners",
+            "relationship": "Husband",
+            "race": "Black",
+            "sex": "Male",
+            "capital_gain": 0,
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States"
+        }
+
+r = requests.post(url, json=sample)
+
+print(f"Status code: {r.status_code}")
+print(f"Body: {r.json()}")
+
